aa5101bb89456030562d4abf0e4ef9893409a978
Andrew Leslie <arleslie@arleslie.com>
2015-03-28 00:09:24 -0400

    Adding global variable & setting up authed vs unauthed home page.

--- index.php
+++ index.php
@@ -5,5 +5,12 @@ error_reporting(E_ALL);
 require('config.inc.php');
 
 $controller = new classes\Controller();
+$SecureChatty = array(
+	'user' => new classes\User();
+);
 
-echo $controller->getPage('login');
+if (!$SecureChatty['user']->isLoggedIn()) {
+	echo $controller->getPage('login');
+} else {
+	echo $controller->getPage('home');
+}
