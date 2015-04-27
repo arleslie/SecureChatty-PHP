e73a8ff01f0958dd9df4f7254ee6c8c81ea3b204
Andrew Leslie <arleslie@arleslie.com>
2015-04-26 20:18:53 -0400

    Fixing undefined variable & removing JS from non-js theme

--- templates/default/home.php
+++ templates/default/home.php
@@ -3,7 +3,7 @@
 		Unread Messages
 	</div>
 	<div class="panel-body">
-		<?php if (count($unreadMessages) !== 0): ?>
+		<?php if (!empty($unreadMessages)): ?>
 			<ul>
 				<?php foreach($unreadMessages as $message): ?>
 					<li><a href="index.php?page=messages&amp;read={$message['id']}">{$message['subject']}</a></li>
--- templates/non-js/header.php
+++ templates/non-js/header.php
@@ -5,7 +5,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 	<link rel="stylesheet" href="style.css">
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<header class="navbar navbar-static-top">
