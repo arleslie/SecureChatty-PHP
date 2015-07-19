fa2a57e4716af13ab2e22ae4b2da16c38bd53b05
Andrew Leslie <arleslie@arleslie.com>
2015-07-19 14:09:16 -0400

    Updating Github URL.

--- templates/non-js/footer.php
+++ templates/non-js/footer.php
@@ -1,6 +1,6 @@
 	</div>
 	<footer class="footer">
-		View our code on GitHub! <a href="https://github.com/arleslie3/SecureChatty-PHP">arleslie3/SecureChatty-PHP</a><br>
+		View our code on GitHub! <a href="https://github.com/arleslie/SecureChatty-PHP">arleslie3/SecureChatty-PHP</a><br>
 		Want to use our javascript site? <a href="index.php?theme=default">Click Here!</a>
 	</footer>
 </body>
