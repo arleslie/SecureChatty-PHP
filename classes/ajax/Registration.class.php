ac523d051e22d4bafd1de2232be0c3509ef7ac97
Andrew Leslie <arleslie@arleslie.com>
2016-03-31 01:46:56 -0400

    Moving bad username rules to a static variable.

--- classes/ajax/Registration.class.php
+++ classes/ajax/Registration.class.php
@@ -6,6 +6,24 @@ class Registration extends Ajax
 {
 	protected $return = array();
 
+	private static $badCharacters = array(
+		',',
+		';',
+		'@',
+		'"',
+		'\'',
+		'%',
+		'*',
+		'&'
+	);
+
+	private static $badUsernames = array(
+		'test', // Reserved for Unit Testing.
+		'administrator',
+		'root',
+		'admin'
+	);
+
 	public function __construct($requests = array())
 	{
 		foreach ($requests as $func => $request) {
@@ -15,21 +33,14 @@ class Registration extends Ajax
 
 	private function checkUsername($username)
 	{
-		$badCharacters = array(
-			',',
-			';',
-			'@',
-			'"',
-			'\'',
-			'%',
-			'*',
-			'&'
-		);
-
-		foreach ($badCharacters as $character) {
+		foreach (self::$badCharacters as $character) {
 			if (stripos($username, $character) !== false) {
 				return 0;
 			}
+		}
+
+		if (in_array($username, self::$badUsernames)) {
+			return 0;
 		}
 
 		if (\classes\User::getUserByUsername($username) !== false) {
